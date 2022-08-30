20960c7523ef5b24357c45543d467070f46ebd47
Aline <line.asramos@gmail.com>
2022-08-30 16:39:46 -0300

    Add ordering to letter guesses

--- src/main/scala/com/wordle/game/LetterGuessResult.scala
+++ src/main/scala/com/wordle/game/LetterGuessResult.scala
@@ -3,20 +3,30 @@ package com.wordle.game
 sealed trait LetterGuessResult {
   val color: String
   val letter: Char
+  val priority: Int
 
   override def toString: String = s"$color$letter${Console.RESET}"
 }
 
+object LetterGuessResult {
+  implicit def ordering(implicit ord: Ordering[Char]): Ordering[LetterGuessResult] =
+    Ordering.by(g => (g.letter, g.priority))
+}
+
 case class LetterNotGuessed(letter: Char) extends LetterGuessResult {
   override val color: String = Console.BLACK
+  override val priority: Int = 40
 }
 
 case class LetterNotFound(letter: Char) extends LetterGuessResult {
   override val color: String = Console.RED
+  override val priority: Int = 30
 }
 case class LetterInWord(letter: Char) extends LetterGuessResult {
   override val color: String = Console.YELLOW
+  override val priority: Int = 20
 }
 case class LetterInPosition(letter: Char, pos: Int) extends LetterGuessResult {
   override val color: String = Console.GREEN
+  override val priority: Int = 10
 }
